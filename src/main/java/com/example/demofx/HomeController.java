db2a83561290f45d74b1d3d3f66eb490e0839c4d
chethanavirajini <cheth.virajini@gmail.com>
2023-11-23 21:25:20 +0530

    update

---
+++ src/main/java/com/example/demofx/HomeController.java
@@ -0,0 +1,87 @@
+package com.example.demofx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void add(ActionEvent event) {
+        try {
+            //catch stage
+            Stage stage = (Stage) this.root.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("save-view.fxml"));
+            //scene load
+            Scene scene = new Scene(fxmlLoader.load());
+
+            //set scene to stage
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+        try {
+            //catch stage
+            Stage stage = (Stage) this.root.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("delete-view.fxml"));
+            //scene load
+            Scene scene = new Scene(fxmlLoader.load());
+
+            //set scene to stage
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void getall(ActionEvent event) {
+        try {
+            //catch stage
+            Stage stage = (Stage) this.root.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("load-view.fxml"));
+            //scene load
+            Scene scene = new Scene(fxmlLoader.load());
+
+            //set scene to stage
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @FXML
+    void update(ActionEvent event) {
+        try {
+            //catch stage
+            Stage stage = (Stage) this.root.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("update-view.fxml"));
+            //scene load
+            Scene scene = new Scene(fxmlLoader.load());
+
+            //set scene to stage
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
--- src/main/java/com/example/demofx/LoginController.java
+++ src/main/java/com/example/demofx/LoginController.java
@@ -2,21 +2,47 @@ package com.example.demofx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
-    private PasswordField txtPassword;
+    private AnchorPane root;
 
     @FXML
     private TextField txtUsername;
 
     @FXML
-    void onLogin(ActionEvent event) {
+    private PasswordField txtPassword;
+
+
+
+    @FXML
+    void login(ActionEvent event) {
         String userName = txtUsername.getText();
         String password = txtPassword.getText();
 
         System.out.println(userName +" " +password);
+
+
+        try {
+            //catch stage
+            Stage stage = (Stage) this.root.getScene().getWindow();
+
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-view.fxml"));
+            //scene load
+            Scene scene = new Scene(fxmlLoader.load());
+
+            //set scene to stage
+            stage.setScene(scene);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
